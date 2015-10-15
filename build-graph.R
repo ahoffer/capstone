8538accc4f4c789070aa06c76df205bac1d79088
AH <github@hoffer.org>
2015-10-15 15:27:13 -0700

    Build simple graph

--- build-graph.R
+++ build-graph.R
@@ -2,8 +2,6 @@ source("packages.R")
 packages("igraph")
 packages("tcltk")
 
-
-
 # names(alldata$user.json)
 # names(alldata$user.json$compliments)
 # names(alldata$user.json$votes)
@@ -12,29 +10,74 @@ packages("tcltk")
 unames = alldata$user.json$name
 length(unique(unames)) == length(unames)
 
+#User nodes
 g = make_empty_graph();
-user = alldata$user.json
-g = g + vertex(user$name, 
-            color="red",
-            yelping_since = as.Date(user$yelping_since, "%Y=%m"),
-            votes_funny = user$votes$funny,
-            votes_useful = user$votes$useful,
-            votes_cool = user$votes$cool,
-            review_count = user$review_count,
-            user_id = user$user_id,
-            fans = user$fans,
-            average_stars = user$average_stars,
-            type = user$type,
-            compliments_funny = user$compliments$funny,
-            compliments_cute = user$compliments$cute,
-            compliments_plain= user$compliments$plain,
-            compliments_writer= user$compliments$writer,
-            compliments_note= user$compliments$note,
-            compliments_hot= user$compliments$hot,
-            compliments_cool= user$compliments$cool,
-            compliments_more= user$compliments$more,
-            compliments_profile= user$compliments$profile,
-            complimenst_photos = user$compliments$photos,
-            compliment_list  = user$compliments$list, 
-            total_compliments = rowSums(user$compliments, na.rm=TRUE)
+objects = alldata$user.json
+g = g + vertex(
+  objects$user_id,
+  color = "red",
+  yelping_since = as.Date(objects$yelping_since, "%Y=%m"),
+  votes_funny = objects$votes$funny,
+  votes_useful = objects$votes$useful,
+  votes_cool = objects$votes$cool,
+  review_count = objects$review_count,
+  realname = objects$name,
+  fans = objects$fans,
+  average_stars = objects$average_stars,
+  type = objects$type,
+  compliments_funny = objects$compliments$funny,
+  compliments_cute = objects$compliments$cute,
+  compliments_plain = objects$compliments$plain,
+  compliments_writer = objects$compliments$writer,
+  compliments_note = objects$compliments$note,
+  compliments_hot = objects$compliments$hot,
+  compliments_cool = objects$compliments$cool,
+  compliments_more = objects$compliments$more,
+  compliments_profile = objects$compliments$profile,
+  complimenst_photos = objects$compliments$photos,
+  compliment_list  = objects$compliments$list,
+  total_compliments = rowSums(objects$compliments, na.rm =
+                                TRUE)
 )
+
+
+#Attribute names
+# sapply(names(alldata$business.json$attributes), USE.NAMES = FALSE, function(name) {
+#   gsub("\\s", "-", name, perl = TRUE)
+# });
+
+
+#Business nodes
+objects = alldata$business.json
+g = g + vertex(
+  objects$business_id,
+  color = "pink",
+  type = objects$type,
+  realname =  objects$name,
+  city = objects$city,
+  state = objects$state,
+  starts = objects$stars,
+  review_count = objects$review_count
+)
+
+#Direct edge from a user to his/her review of a business
+r = alldata$review.json
+reviews = r[is.element(r$user_id, V(g)$name) & is.element(r$business_id, V(g)$name),]
+print(paste("Creating", nrow(reviews), "reviews"))
+
+#Convert NA to zero
+reviews$votes[is.na(reviews$votes)] = 0
+
+g = g +    edge(
+  reviews$user_id,
+  reviews$business_id,
+  color = "gray",
+  votes_funny = reviews$votes$funny,
+  votes_useful = reviews$votes$useful,
+  votes_cool = reviews$votes$cool,
+  name = reviews$review_id,
+  stars = reviews$stars,
+  date = as.POSIXct(reviews$date)
+)
+
+
