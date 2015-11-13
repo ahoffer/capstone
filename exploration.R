1010264c0383068cd4d54c374ab6396fa07d5837
Aaron Hoffer <github@hoffer.org>
2015-11-12 22:11:10 -0700

    Do histograms or degrees, connectivity, other things.

--- exploration.R
+++ exploration.R
@@ -1,5 +1,5 @@
 #Exploration
-g=graph_copy
+g = graph_copy
 
 #scrub city names
 V(g)$city = tolower(V(g)$city)
@@ -9,7 +9,7 @@ V(g)$city = tolower(V(g)$city)
 # cities = cities[!is.na(cities)]
 
 #Look at the Chandler area
-chandler.vector= (V(g)$type=="user") | (V(g)$city =='chandler')
+chandler.vector = (V(g)$type == "user") | (V(g)$city == 'chandler')
 chandler.graph = g - V(g)[!chandler.vector]
 
 #Removes uses who have no friends and have not reviewed a business
@@ -27,16 +27,14 @@ ecount(chandler.graph)
 #Still too many nodes and edges to run most graph
 #analysis algorithms
 #Reduce graph size to ego nets around businesses.
-ego.nets = ego(
-  chandler.graph, 
-  nodes=V(chandler.graph)[type != "user"],
-  order=1)
+ego.nets = ego(chandler.graph,
+               nodes = V(chandler.graph)[type != "user"],
+               order = 1)
 
 #Typify the ego nets of the business to establish a baseline
-
-x= induced_subgraph(chandler.graph, ego.nets[1])
-
-subgraphs = sapply(ego.nets, function(vs) {
- chandler.graph - vs
+subgraphs = lapply(ego.nets[1:10], function(vs) {
+  induced.subgraph(chandler.graph, vs)
 })
+
+degrees = sapply(subgraphs, degree)
 
