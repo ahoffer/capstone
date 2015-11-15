f84c3fe757846abd9df575935bdad9ac4065646e
Aaron Hoffer <github@hoffer.org>
2015-11-15 14:25:33 -0700

    Calculate averge number of edges, nodes. Next, calculate average fun/fun.

--- exploration.R
+++ exploration.R
@@ -32,9 +32,38 @@ ego.nets = ego(chandler.graph,
                order = 1)
 
 #Typify the ego nets of the business to establish a baseline
-subgraphs = lapply(ego.nets[1:10], function(vs) {
+subgraphs = lapply(ego.nets, function(vs) {
   induced.subgraph(chandler.graph, vs)
 })
 
-degrees = sapply(subgraphs, degree)
+average.number.nodes = mean(sapply(subgraphs, vcount))
+average.number.edges = mean(sapply(subgraphs, ecount))
 
+#Investigate the degrees of the different ego nets
+degrees = sapply(subgraphs, degree)
+average.degrees = sapply(degrees, mean)
+hist(average.degrees)
+quantile(average.degrees, seq(0, 1, 0.1))
+#For ego.net where order = 1 
+# 0%       10%       20%       30%       40%       50% 
+#   1.000000  1.500000  1.600000  1.666667  1.777778  1.885621 
+# 60%       70%       80%       90%      100% 
+# 2.000000  2.228205  2.469804  3.000000 14.585366 
+
+
+#Expand ego nets to 2 edges out from the businesses
+ego.nets2 = ego(chandler.graph,
+               nodes = V(chandler.graph)[type != "user"],
+               order = 2)
+
+subgraphs2 = lapply(ego.nets2, function(vs) {
+  induced.subgraph(chandler.graph, vs)
+})
+
+quantile(
+  sapply(sapply(subgraphs2, degree), mean), 
+  seq(0, 1, 0.1))
+# 0%        10%        20%        30%        40%        50%        60% 
+# 1.000000   2.903812   4.571429   8.173147  15.020925  25.569113  36.095102 
+# 70%        80%        90%       100% 
+#   45.224641  54.581658  67.746356 123.911243 
